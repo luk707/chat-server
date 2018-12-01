1ae4df6b4a586893bda29296fbcb3bba0c6bbdbb
Luke Harris <lukeharris01170@gmail.com>
2018-12-01 21:25:00 +0000

    Configure babel runtime and async await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const pause = time =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
+
+(async () => {
+  console.log("Hello ...");
+  await pause(1000);
+  console.log("World!");
+})();
